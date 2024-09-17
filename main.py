4e21fe74966bfe949206677cdcda9a5751a5fe19
Ben Shutt <shuttbenjamin@gmail.com>
2024-09-17 11:34:53 +0100

    Import and use yt-dlp WIP

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 """
 1. Reads a new-line separated file of Youtube URLs to download as MP3 files (read_dir)
-2. Downloads into a temporary directory using youtube-dl (https://github.com/ytdl-org/youtube-dl)
+2. Downloads into a temporary directory using yt-dlp (https://github.com/yt-dlp/yt-dlp)
 3. Moves downloaded files into the Swim Pro directory (write_dir).
 
 Example usage (arguments are defaulted):
@@ -15,6 +15,8 @@ import argparse
 import subprocess
 import tempfile
 
+from yt_dlp import YoutubeDL
+
 # User's home directory
 HOME = os.path.expanduser("~")
 
@@ -23,9 +25,6 @@ DEFAULT_READ_FILE = f"{HOME}/Downloads/youtube-urls.txt"
 
 # Default directory to move the downloaded MP3 files into
 DEFAULT_WRITE_DIR = "/Volumes/SWIM PRO"
-
-# YouTube download command
-COMMAND = "youtube-dl"
 
 # ==================== Arguments ====================
 
@@ -49,31 +48,14 @@ def init_arguments():
     
     return (read_file, write_dir)
 
-# ==================== Command ====================
-    
-def assert_command_exists(exe):
-    command = f"command -v {exe} &> /dev/null"
-    exists = subprocess.call(command, shell=True) == 0
-    if not exists:
-        raise SystemExit(f"{exe} command does not exist")
-
 # ==================== Read ====================
 
 def read_urls(read_file):
     with open(read_file, "r") as file:
         lines = [line.strip() for line in file if line != ""]
         return list(filter(lambda x: x != "", lines))
+    
 
-# ==================== Download ====================
-
-# TODO: Just import youtube_dl directly!
-def download(url):
-    destination=f"{HOME}/file.mp3"
-    print(destination)
-    command = f"{COMMAND} --verbose --extract-audio --audio-format mp3 -o {destination} {url}"
-    result = subprocess.call(command, shell=True) == 0
-    if not result:
-        raise SystemExit(f"Failed to download {url}")
     
 # ==================== Main ====================
 
@@ -81,15 +63,21 @@ def main():
     # Initialize the command-line arguments
     (read_file, write_dir) = init_arguments()
     
-    # Check the youtube-dl command exists
-    assert_command_exists(COMMAND)
-    
     # Extract the URLs from the file to read
     urls = read_urls(read_file)
     
+    # Specify the configuration for the download
+    youtube_dl_options = {
+        'format': 'mp4/bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'm4a',
+        }]
+    }
+
     # Download each URL
-    for url in urls:
-        download(url)
+    with YoutubeDL(youtube_dl_options) as youtube_dl:
+        youtube_dl.download(urls)
     
 if __name__ == "__main__":
     main()
